1590ed4291ef9dca16fff14fd4180176a62b5e86
dineka89 <dineka89@inbox.ru>
2023-01-14 14:42:32 +0300

    222

---
+++ src/main/java/ru/project/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package ru.project.repository;
+
+import ru.project.model.User;
+
+import java.util.List;
+
+public interface UserRepository {
+
+    List<User> getAllUsers();
+
+    void createUser(User user);
+
+    void updateUser(User user);
+
+    User readUser(long id);
+
+    User deleteUser(long id);
+}
---
+++ src/main/java/ru/project/service/UserService.java
@@ -0,0 +1,16 @@
+package ru.project.service;
+
+import ru.project.model.User;
+
+import java.util.List;
+
+public interface UserService {
+
+    List<User> getAllUsers();
+
+    User readUser(long id);
+
+    User deleteUser(long parseUnsignedInt);
+
+    void createOrUpdateUser(User user);
+}
