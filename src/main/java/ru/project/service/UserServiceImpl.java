55a71cbd9ac1084dfa11dfd7f77c9a044fe4fe0f
dineyra <dineyra06@gmail.com>
2023-01-23 12:49:58 +0300

    231

--- src/main/java/ru/project/service/UserServiceImpl.java
+++ src/main/java/ru/project/service/UserServiceImpl.java
@@ -1,14 +1,12 @@
 package ru.project.service;
 
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import ru.project.model.User;
 import ru.project.repository.UserRepository;
 
 import java.util.List;
 
 @Service
-@Transactional
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
